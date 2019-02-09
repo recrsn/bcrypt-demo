be8bb1de994d312177f7a4a9f305a6567dc1bb0b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-02-09 15:20:11 +0530

    v3.0.4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const logger = require('morgan');
+const { NotFound, BadRequest } = require('http-errors');
 
 const production = process.env.NODE_ENV === 'production';
 
@@ -10,26 +11,86 @@ const app = express();
 app.use(logger(production ? 'combined' : 'dev'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.post('/bcrypt', (req, res) => {
-    bcrypt.hash(req.body.text, 13)
-        .then(hash => {
-            res.json({
-                hash
-            });
-        }).catch(error => {
-            res.status(500).json({
-                error
-            });
-        });
+app.post('/encode', (req, res, next) => {
+  const data = req.body.data;
+  let rounds = req.body.rounds;
+
+  if (!data) {
+    return next(new BadRequest('data is required'));
+  }
+
+  if (!rounds) {
+    return next(new BadRequest('rounds is requrired'));
+  }
+
+  rounds = Number.parseInt(rounds, 10);
+
+  if (Number.isNaN(rounds)) {
+    return next(new BadRequest('rounds must be a number'));
+  }
+
+  const start = Date.now();
+  bcrypt
+    .hash(data, rounds)
+    .then(hash => {
+      res.json({
+        hash,
+        time: Date.now() - start
+      });
+    })
+    .catch(error => {
+      return next(new BadRequest(error.message));
+    });
+});
+
+app.post('/decode', (req, res, next) => {
+  const { data, hash } = req.body;
+
+  if (!data) {
+    return next(new BadRequest('data is required'));
+  }
+
+  if (!hash) {
+    return next(new BadRequest('hash is requrired'));
+  }
+
+  const start = Date.now();
+  bcrypt
+    .compare(data, hash)
+    .then(result => {
+      res.json({
+        result,
+        time: Date.now() - start
+      });
+    })
+    .catch(error => {
+      return next(new BadRequest(error.message));
+    });
+});
+
+app.use(function(req, res, next) {
+  return next(new NotFound());
+});
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = status < 500 ? error.message : 'Internal server error';
+
+  if(status >= 500) {
+      console.error(error);
+  }
+
+  res.status(status).json({ error: message });
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.error(err);
-    }
-    console.log('Server started on port ' + port);
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Server started on port ' + port);
 });
--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,95 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+function htmlEscape(str) {
+  return str
+    .toString()
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\//g, '&#x2F;');
+}
 
-    const text = document.getElementById('text').value;
+const encodeResult = document.getElementById('encodeResult');
+const verifyResult = document.getElementById('verifyResult');
 
-    fetch('/bcrypt', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            text
-        })
-    }).then(response => response.json())
-        .then(data => {
-            document.getElementById('hash').innerHTML = data.hash;
-        });
-})
+const renderError = error => `<span class="text-danger">
+<i class="fas fa-exclamation-circle"></i><span class="error-text ml-1">${htmlEscape(
+  error
+)}</span>
+</span>`;
+
+const renderHash = response =>
+  `<p class="hash bg-light p-4"><code>${htmlEscape(response.hash)}</code></p>
+  <div class="time text-muted"><i class="fa fa-clock"></i><small class="ml-1">${htmlEscape(
+    response.time
+  )} ms</small></div>`;
+
+function renderCompare(response) {
+  const res = response.result
+    ? `<p class="text-success"><i class="fas fa-check"></i><span class="ml-1">Match</span><p>`
+    : `<p class="text-danger"><i class="fas fa-cross"></i><span class="ml-1">Does not match</span></p>`;
+
+  return `
+    ${res}
+    <div class="time text-muted"><i class="fa fa-clock"></i><small class="ml-1">${htmlEscape(
+      response.time
+    )} ms</small></div>`;
+}
+
+document.getElementById('encodeForm').addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const data = document.getElementById('plaintext').value;
+  const rounds = parseInt(document.getElementById('rounds').value, 10);
+
+  try {
+    const req = await fetch('/encode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data,
+        rounds
+      })
+    });
+
+    const response = await req.json();
+    if (req.ok) {
+      encodeResult.innerHTML = renderHash(response);
+    } else {
+      encodeResult.innerHTML = renderError(response.error);
+    }
+  } catch (error) {
+    encodeResult.innerHTML = renderError('An unexpected error occured');
+  }
+});
+
+document.getElementById('decodeForm').addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const data = document.getElementById('text').value;
+  const hash = document.getElementById('hash').value;
+
+  try {
+    const req = await fetch('/decode', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data,
+        hash
+      })
+    });
+
+    const response = await req.json();
+    if (req.ok) {
+      verifyResult.innerHTML = renderCompare(response);
+    } else {
+      verifyResult.innerHTML = renderError(response.error);
+    }
+  } catch (error) {
+    verifyResult.innerHTML = renderError('An unexpected error occured');
+  }
+});
